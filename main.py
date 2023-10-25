5f3c5b30324ca1ac6ae4471ae8f15bb2121bee16
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-25 14:18:51 +0300

    bot with one  botton

--- main.py
+++ main.py
@@ -10,60 +10,32 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.markdown import hbold
 from dotenv import load_dotenv
 
 load_dotenv()
 
 token = os.getenv("token")
-
-nlp = spacy.load("ru_core_news_sm")
-
-# Конвертация PDF в текст
-def convert_pdf_to_text(file_path):
-    with open(file_path, "rb") as file:
-        reader = PyPDF2.PdfReader(file)
-        text = ""
-        for page in range(len(reader.pages)):
-            text += reader.pages[page].extract_text()
-    return text
-
-# Поиск фрагмента текста
-def find_relevant_excerpt(text, query):
-    doc = nlp(text)
-    sentences = [sent.text.strip() for sent in doc.sents]
-    
-    max_similarity = 0
-    most_relevant_sentence = ""
-    
-    for sentence in sentences:
-        similarity = nlp(sentence).similarity(nlp(query))
-        if similarity > max_similarity:
-            max_similarity = similarity
-            most_relevant_sentence = sentence
-            
-    return most_relevant_sentence
-
-text_from_pdf = convert_pdf_to_text("this.pdf")
-
-# Инициализация бота и диспетчера
 dp = Dispatcher()
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}! Введите запрос и я найду для вас информацию в инструкции.")
+    # Создание инлайн клавиатуры
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[])
 
-
-@dp.message()
-async def echo_handler(message: types.Message) -> None:
-    user_query = message.text
-    response = find_relevant_excerpt(text_from_pdf, user_query)
     
-    if response:
-        await message.answer(response)
-    else:
-        await message.answer("Извините, я не нашел подходящей информации по вашему запросу.")
+    # Добавление 11 пустых кнопок
+    button = InlineKeyboardButton(text="Some Text", callback_data="some_data")
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button]])
+
+
+    # Отправка сообщения с инлайн клавиатурой
+    await message.answer(
+        f"Hello, {hbold(message.from_user.full_name)}! Выберите раздел инструкции.",
+        reply_markup=keyboard
+    )
 
 
 async def main() -> None:
