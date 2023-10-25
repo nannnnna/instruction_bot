b76069cf561a6e3ee7188c2c74955806518bc7a7
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-25 16:28:34 +0300

    bot with menu text

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import asyncio
 import logging
 import sys
 import os
-from os import getenv
-
 import PyPDF2
-import spacy
+import re
+
+from os import getenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
@@ -19,6 +19,35 @@ load_dotenv()
 token = os.getenv("token")
 dp = Dispatcher()
 
+#text and image from pdf 
+
+def convert_pdf_to_text_range(file_path, start_page, end_page):
+    with open(file_path, "rb") as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ""
+        for page in range(start_page-1, end_page): # индексация страниц начинается с 0
+            text += reader.pages[page].extract_text()
+    return text
+
+text_from_inst= convert_pdf_to_text_range("this.pdf", 2, 3)
+text_from_oper= convert_pdf_to_text_range("oper.pdf", 2, 2)
+# Удаление последовательности точек
+cleaned_text_inst = re.sub(r'\.{2,}', '', text_from_inst)
+cleaned_text_oper = re.sub(r'\.{2,}', '', text_from_oper)
+# print(cleaned_text)
+# Функция для фильтрации текста по заданному шаблону
+def filter_by_pattern(text):
+    return "\n".join([line for line in text.split("\n") if re.match(r'^\d+\.', line)])
+
+filtered_text_i = filter_by_pattern(cleaned_text_inst)
+filtered_text_o = filter_by_pattern(cleaned_text_oper)
+print(filtered_text_i)
+print(filtered_text_o)
+# for page_num, text in enumerate(texts_from_pdf, 1): вывод всего текста с допоплнительной нумерацией страниц
+#     print(f"------ Page {page_num} ------")
+#     print(text)
+#     print("-----------------------------\n")
+
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
@@ -27,8 +56,9 @@ async def command_start_handler(message: Message) -> None:
 
     
     # Добавление 11 пустых кнопок
-    button = InlineKeyboardButton(text="Some Text", callback_data="some_data")
-    keyboard = InlineKeyboardMarkup(inline_keyboard=[[button]])
+    for _ in range(11):
+        button = InlineKeyboardButton(text=" ", callback_data="empty_data")
+        keyboard.inline_keyboard.append([button])
 
 
     # Отправка сообщения с инлайн клавиатурой
