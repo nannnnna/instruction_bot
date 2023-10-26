b74a87566f40009d6437e56db192263453887b4f
nannnnna <141238876+nannnnna@users.noreply.github.com>
2023-10-26 16:50:46 +0300

    this is bot which show text of instruction

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import sys
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils import executor
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.types import ParseMode
 from os import getenv
 from PIL import Image, ImageDraw, ImageFont
 from dotenv import load_dotenv
@@ -25,7 +27,6 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=token)
 dp = Dispatcher(bot)
-
 button_press_count = {"администратор": 0, "оператор": 0}
 
 
@@ -39,27 +40,102 @@ def convert_pdf_to_text_range(file_path, start_page, end_page):
     return text
 
 text_from_inst= convert_pdf_to_text_range("this.pdf", 2, 3)
+# print(text_from_inst)
+text_inst_admi= convert_pdf_to_text_range("this.pdf", 1, 45)
+text_inst_oper= convert_pdf_to_text_range("oper.pdf", 1, 18)
 text_from_oper= convert_pdf_to_text_range("oper.pdf", 2, 2)
+# print(text_inst_admi)
+def split_text_to_pages(text):
+    pages = re.split(r'Руководство администратора\s+Стр\. \d+ из \d+', text)
+    pages_dict = {}
+
+    for i, page in enumerate(pages):
+        page = page.strip()
+
+        if page:
+            pages_dict[i+1] = page
+
+    return pages_dict
+
+pages_dict_i = split_text_to_pages(text_inst_admi)
+# print(pages_dict_i)
+admin_pressed = False  # Переменная для проверки, нажата ли кнопка "администратор"
+def split_text_to_pages(text):
+    pages = re.split(r'Руководство оператора\s+Стр\. \d+ из \d+', text)
+    pages_dict = {}
+
+    for i, page in enumerate(pages):
+        page = page.strip()
+
+        if page:
+            pages_dict[i+1] = page
+
+    return pages_dict
+
+pages_dict_o = split_text_to_pages(text_inst_oper)
+print(pages_dict_o)
 # Удаление последовательности точек
 cleaned_text_inst = re.sub(r'\.{2,}', '', text_from_inst)
+# print(cleaned_text_inst)
 cleaned_text_oper = re.sub(r'\.{2,}', '', text_from_oper)
 # print(cleaned_text)
+
 # Функция для фильтрации текста по заданному шаблону
 def filter_by_pattern(text):
     return "\n".join([line for line in text.split("\n") if re.match(r'^\d+\.\s[^.].*', line)])
 
 filtered_text_i = filter_by_pattern(cleaned_text_inst)
+# print(filtered_text_i)
 filtered_text_o = filter_by_pattern(cleaned_text_oper)
 lines_i = filtered_text_i.split('\n') # text for second buttons
 lines_o = filtered_text_o.split('\n')
 # print(lines_i)
+new_lines_i = []
+
+for i in range(len(lines_i)):
+    match = re.search(r"(\d+)\s*$", lines_i[i])  # Ищем номер страницы в конце строки, учитывая возможные пробелы.
+    
+    if match:
+        start_page = int(match.group(1))
+        
+        if i < len(lines_i) - 1:  # Если это не последний элемент
+            next_match = re.search(r"(\d+)\s*$", lines_i[i+1])  # Ищем номер страницы в следующей строке, учитывая возможные пробелы.
+            end_page = int(next_match.group(1)) - 1 if next_match else start_page
+        else:
+            end_page = start_page
+        
+        # Убираем начальный номер и точку, а также обновляем номера страниц.
+        text = re.sub(r"^\d+\.\s+", "", lines_i[i])
+        text = re.sub(r"(\d+)\s*$", f"{start_page}-{end_page}", text)
+        
+        new_lines_i.append(text)
+
+# print(new_lines_i)
 # print(lines_o)
+new_lines_o = []
+
+for i in range(len(lines_o)):
+    match = re.search(r"(\d+)\s*$", lines_o[i])  # Ищем номер страницы в конце строки, учитывая возможные пробелы.
+    
+    if match:
+        start_page = int(match.group(1))
+        
+        if i < len(lines_o) - 1:  # Если это не последний элемент
+            next_match = re.search(r"(\d+)\s*$", lines_o[i+1])  # Ищем номер страницы в следующей строке, учитывая возможные пробелы.
+            end_page = int(next_match.group(1)) - 1 if next_match else start_page
+        else:
+            end_page = start_page
+        
+        # Убираем начальный номер и точку, а также обновляем номера страниц.
+        text = re.sub(r"^\d+\.\s+", "", lines_o[i])
+        text = re.sub(r"(\d+)\s*$", f"{start_page}-{end_page}", text)
+        
+        new_lines_o.append(text)
+
+# print(new_lines_o)
 # print(filtered_text_i)
 # print(filtered_text_o)
-for page_num, text in enumerate(text_from_inst, 1): #вывод всего текста с допоплнительной нумерацией страниц
-    print(f"------ Page {page_num} ------")
-    print(text)
-    print("-----------------------------\n")
+
 
 #work eith image 
 def extract_and_save_images_from_pdf_with_pymupdf(pdf_path, output_file):
@@ -141,10 +217,11 @@ def extract_images_from_combined_file(image_path, page_number, page_heights, out
 #     oper_image_path = extract_images_from_combined_file('C:/Users/79819/Documents/GitHub/instruction_bot/oper_images.png', page_number, page_heights, output_folder)
 #     # print(f"Изображение для {page_number} страницы сохранено как: {oper_image_path}")
 
-def make_keyboard_from_lines(lines):
+# Функция для создания клавиатуры
+def make_keyboard_from_lines(role, lines):
     keyboard = InlineKeyboardMarkup()
-    for line in lines:
-        button = InlineKeyboardButton(line, callback_data=line)
+    for i, line in enumerate(lines):
+        button = InlineKeyboardButton(line, callback_data=f"{role}_{i}")
         keyboard.add(button)
     return keyboard
 
@@ -163,7 +240,7 @@ async def send_welcome(message: types.Message):
 @dp.callback_query_handler(lambda c: c.data == "admin")
 async def process_admin_callback(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    keyboard = make_keyboard_from_lines(lines_i)
+    keyboard = make_keyboard_from_lines("admin",new_lines_i)
     await bot.send_message(callback_query.from_user.id, "Выберите элемент:", reply_markup=keyboard)
     print(1)
 
@@ -171,15 +248,35 @@ async def process_admin_callback(callback_query: types.CallbackQuery):
 @dp.callback_query_handler(lambda c: c.data == "operator")
 async def process_operator_callback(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
-    keyboard = make_keyboard_from_lines(lines_o)
+    keyboard = make_keyboard_from_lines("operator",new_lines_o)
     await bot.send_message(callback_query.from_user.id, "Выберите элемент:", reply_markup=keyboard)
     print(2) 
 
-# Обработчик CallbackQuery для элементов lines_i и lines_o
-@dp.callback_query_handler(lambda c: c.data in lines_i or c.data in lines_o)
+# Обработчик CallbackQuery для элементов new_lines_i и new_lines_o
+@dp.callback_query_handler(lambda c: c.data.startswith("admin_") or c.data.startswith("operator_"))
 async def process_item_callback(callback_query: types.CallbackQuery):
-    await bot.answer_callback_query(callback_query.id, text=f"Вы выбрали: {callback_query.data}")
-    logging.info(f"Выбрано: {callback_query.data}")
+    await bot.answer_callback_query(callback_query.id)
+    
+    role, item_index = callback_query.data.split("_")
+    item_index = int(item_index)
+    
+    selected_item = new_lines_i[item_index] if role == "admin" else new_lines_o[item_index]
+    pages_dict = pages_dict_i if role == "admin" else pages_dict_o
+    
+    # Извлекаем номера страниц из выбранной строки
+    match = re.search(r"(\d+)-(\d+)", selected_item)
+    if match:
+        start_page, end_page = map(int, match.groups())
+        
+        # Извлекаем и объединяем текст со всех страниц в диапазоне
+        pages_text = ""
+        for page in range(start_page, end_page+1):
+            pages_text += pages_dict.get(page, "") + "\n"
+        
+        # Отправляем текст пользователю
+        await bot.send_message(callback_query.from_user.id, pages_text or "Текст отсутствует.")
+    else:
+        await bot.send_message(callback_query.from_user.id, "Не удалось найти диапазон страниц.")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
